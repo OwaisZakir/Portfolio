210a795b5b8c3b62bbf3bf77bed9a6c90ea41cc6
owaiszakir <owaiszakir88@gmail.com>
2025-02-04 18:56:01 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/style.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Testing</h1>
+    </div>
+  );
+};
+
+export default App;
